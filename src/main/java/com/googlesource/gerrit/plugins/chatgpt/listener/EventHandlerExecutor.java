1b8c5e92731f1b358cb9763d4b5a895ea5836d61
Dariusz Luksza <dariusz.luksza@gmail.com>
2024-04-23 16:39:27 +0100

    Decouple thread pool from event handler
    Currently handling each event will result in a single thread
    `ExecutorService` being created to use
    `CompletableFuture.runAsync` on it. What's more, each
    `EventListenerHandler` will add a JVM shutdown hook, which will later
    prevent that instance from being GC'ed and lead to slow memory build-up
    on the system.
    
    To prevent memory leakage we extract the thread pool into the
    `EventHandlerExecutor` class plus we register a `LifecycleListener` in
    form of `PluginLifecycleListener` to register and unregister JVM
    shutdown hook, and stop the thread pool when the plugin is unloaded.
    
    The `EventListenerHandler` is also renamed to `EventHandlerTask` and
    stays mostly unchanged.
    
    This refactoring has one side effect, we cannot rely on the
    `SingletonManager` in tests, as the singleton instance for change will
    be unregistered by the `EventHandlerExecutor` before we can even access
    it. To fix this a follow-up change will be provided to replace
    `SingletonManger` with Guice scope.
    
    Change-Id: If9422a4449311682958173901b438dd847e69acb

---
+++ src/main/java/com/googlesource/gerrit/plugins/chatgpt/listener/EventHandlerExecutor.java
@@ -0,0 +1,43 @@
+package com.googlesource.gerrit.plugins.chatgpt.listener;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.google.gerrit.server.events.Event;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.chatgpt.config.Configuration;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Singleton
+public class EventHandlerExecutor {
+    private final ExecutorService executor;
+    private final EventHandlerTask.Factory taskHandlerFactory;
+
+    @Inject
+    EventHandlerExecutor(EventHandlerTask.Factory taskHandlerFactory) {
+        this.taskHandlerFactory = taskHandlerFactory;
+        this.executor = Executors.newCachedThreadPool(
+                new ThreadFactoryBuilder().setNameFormat("ChatGPT request executor").build());
+    }
+
+    public void execute(Configuration config, Event event) {
+        executor.execute(taskHandlerFactory.create(config, event));
+    }
+
+    void shutdown() {
+        log.info("shutting down execution queue");
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException ex) {
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+}
