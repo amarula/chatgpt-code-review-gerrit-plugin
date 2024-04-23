931044225bf5aa9fbe56dd811b33ddbe0428a938
Dariusz Luksza <dariusz.luksza@gmail.com>
2024-04-23 16:39:27 +0100

    Decouple thread pool from event handler
    Currently handling each event will result in a single thread
    `ExecutorService` being created to use
    `CompletableFuture.runAsync` on it. What's more, each
    `EventListenerHandler` will add a JVM shutdown hook, which will later
    prevent that instance from being GC'ed and lead to slow memory build-up
    on the system.
    
    To prevent memory leakage we extract the thread pool into the
    `EventHandlerExecutor` class plus we register a `LifecycleListener` in
    form of `PluginLifecycleListener` to register and unregister JVM
    shutdown hook, and stop the thread pool when the plugin is unloaded.
    
    The `EventListenerHandler` is also renamed to `EventHandlerTask` and
    stays mostly unchanged.
    
    This refactoring has one side effect, we cannot rely on the
    `SingletonManager` in tests, as the singleton instance for change will
    be unregistered by the `EventHandlerExecutor` before we can even access
    it. To fix this a follow-up change will be provided to replace
    `SingletonManger` with Guice scope.
    
    Change-Id: If9422a4449311682958173901b438dd847e69acb

---
+++ src/main/java/com/googlesource/gerrit/plugins/chatgpt/listener/PluginLifecycleListener.java
@@ -0,0 +1,37 @@
+package com.googlesource.gerrit.plugins.chatgpt.listener;
+
+import com.google.gerrit.extensions.events.LifecycleListener;
+import com.google.gerrit.lifecycle.LifecycleModule;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class PluginLifecycleListener implements LifecycleListener {
+    public static final LifecycleModule MODULE = new LifecycleModule() {
+        @Override
+        protected void configure() {
+            listener().to(PluginLifecycleListener.class);
+        }
+    };
+
+    private final Thread shutdownHook;
+    private final EventHandlerExecutor executor;
+
+    @Inject
+    PluginLifecycleListener(EventHandlerExecutor executor) {
+        this.shutdownHook = new Thread(executor::shutdown);
+        this.executor = executor;
+    }
+
+    @Override
+    public void start() {
+        Runtime.getRuntime().addShutdownHook(shutdownHook);
+    }
+
+    @Override
+    public void stop() {
+        Runtime.getRuntime().removeShutdownHook(shutdownHook);
+        executor.shutdown();
+    }
+}
