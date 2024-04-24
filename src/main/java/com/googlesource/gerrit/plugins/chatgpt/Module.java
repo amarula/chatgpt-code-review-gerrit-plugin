6f442f36703f0b03b3e92e3ed00eafb86c49453d
Dariusz Luksza <dariusz.luksza@gmail.com>
2024-04-24 13:10:12 +0100

    Initial add of GerritEventContextModule
    Slowly migrate to Guice injection by introducing a
    `GerritEventContectModule` and creating a child injector with bindings
    for shared instances.
    
    Change-Id: Iac7209cd3fea208fb956e23f338829b57943a61a

--- src/main/java/com/googlesource/gerrit/plugins/chatgpt/Module.java
+++ src/main/java/com/googlesource/gerrit/plugins/chatgpt/Module.java
@@ -3,7 +3,6 @@ package com.googlesource.gerrit.plugins.chatgpt;
 import com.google.gerrit.server.events.EventListener;
 import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
-import com.googlesource.gerrit.plugins.chatgpt.listener.EventHandlerTask;
 import com.googlesource.gerrit.plugins.chatgpt.listener.GerritListener;
 import com.googlesource.gerrit.plugins.chatgpt.listener.PluginLifecycleListener;
 
@@ -11,7 +10,6 @@ public class Module extends AbstractModule {
 
     @Override
     protected void configure() {
-        install(EventHandlerTask.MODULE);
         install(PluginLifecycleListener.MODULE);
 
         Multibinder<EventListener> eventListenerBinder = Multibinder.newSetBinder(binder(), EventListener.class);
