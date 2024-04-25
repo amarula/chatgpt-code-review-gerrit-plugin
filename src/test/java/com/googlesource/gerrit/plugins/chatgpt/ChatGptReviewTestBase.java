540fbc3e10917babc42963d5c85949ebf8e53070
Jacek Centkowski <geminica.programs@gmail.com>
2024-04-25 09:38:05 +0200

    Migrate GerritClientPatchSetStateless to direct GerritApi
    The corresponing tests were updated to mock required GerritApi
    endpoints.
    
    Change-Id: I13cbfc773ae9e44ab74928144092f49fd26845b5

--- src/test/java/com/googlesource/gerrit/plugins/chatgpt/ChatGptReviewTestBase.java
+++ src/test/java/com/googlesource/gerrit/plugins/chatgpt/ChatGptReviewTestBase.java
@@ -53,6 +53,7 @@ import org.mockito.Mockito;
 import org.mockito.stubbing.Answer;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -246,6 +247,11 @@ public class ChatGptReviewTestBase {
     protected <T> T readTestFileToClass(String filename, Class<T> clazz) {
         Gson gson = OutputFormat.JSON.newGson();
         return gson.fromJson(readTestFile(filename), clazz);
+    }
+
+    protected <T> T readTestFileToType(String filename, Type type) {
+        Gson gson = OutputFormat.JSON.newGson();
+        return gson.fromJson(readTestFile(filename), type);
     }
 
     protected String readTestFile(String filename) {
