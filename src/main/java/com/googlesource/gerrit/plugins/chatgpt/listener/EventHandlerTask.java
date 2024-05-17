amarula/chatgpt-code-review-gerrit-plugin

commit ec1cf3b0519d03c30b8e2d74cf1033218e8628da

Bind GerritChange in event context
Change-Id: I80ca16fdb760b19322ca2ad142c6e07fb3c47f1d

src/main/java/com/googlesource/gerrit/plugins/chatgpt/listener/EventHandlerTask.java
@@ -3,13 +3,9 @@ package com.googlesource.gerrit.plugins.chatgpt.listener;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Splitter;
 import com.google.gerrit.extensions.client.ChangeKind;
-import com.google.gerrit.extensions.config.FactoryModule;
 import com.google.gerrit.server.data.ChangeAttribute;
 import com.google.gerrit.server.data.PatchSetAttribute;
-import com.google.gerrit.server.events.Event;
 import com.google.inject.Inject;
-import com.google.inject.Module;
-import com.google.inject.assistedinject.Assisted;
 import com.googlesource.gerrit.plugins.chatgpt.PatchSetReviewer;
 import com.googlesource.gerrit.plugins.chatgpt.config.Configuration;
 import com.googlesource.gerrit.plugins.chatgpt.data.ChangeSetDataHandler;
@@ -30,17 +26,6 @@ import static com.google.gerrit.extensions.client.ChangeKind.REWORK;
 
 @Slf4j
 public class EventHandlerTask implements Runnable {
-    public static final Module MODULE = new FactoryModule() {
-        @Override
-        protected void configure() {
-            factory(EventHandlerTask.Factory.class);
-        }
-    };
-
-    public interface Factory {
-        EventHandlerTask create(Event event);
-    }
-
     @VisibleForTesting
     public enum Result {
         OK, NOT_SUPPORTED, FAILURE
@@ -51,26 +36,26 @@ public class EventHandlerTask implements Runnable {
             "comment-added", true
     );
 
-    private final Event event;
     private final Configuration config;
     private final GerritClient gerritClient;
     private final GitRepoFiles gitRepoFiles;
+    private final GerritChange change;
     private final PatchSetReviewer reviewer;
     private final PluginDataHandler pluginDataHandler;
 
     @Inject
     EventHandlerTask(
             Configuration config,
+            GerritChange change,
             PatchSetReviewer reviewer,
             GerritClient gerritClient,
             GitRepoFiles gitRepoFiles,
-            PluginDataHandler pluginDataHandler,
-            @Assisted Event event
+            PluginDataHandler pluginDataHandler
     ) {
+        this.change = change;
         this.reviewer = reviewer;
         this.gerritClient = gerritClient;
         this.config = config;
-        this.event = event;
         this.gitRepoFiles = gitRepoFiles;
         this.pluginDataHandler = pluginDataHandler;
     }
@@ -82,7 +67,6 @@ public class EventHandlerTask implements Runnable {
 
     @VisibleForTesting
     public Result execute() {
-        GerritChange change = new GerritChange(event);
         gerritClient.initialize(config, change);
         Integer gptAccountId = gerritClient.getNotNullAccountId(change, config.getGerritUserName());
         ChangeSetData changeSetData = ChangeSetDataHandler.getNewInstance(config, change, gptAccountId);

src/main/java/com/googlesource/gerrit/plugins/chatgpt/listener/GerritEventContextModule.java
@@ -1,19 +1,22 @@
 package com.googlesource.gerrit.plugins.chatgpt.listener;
 
 import com.google.gerrit.extensions.config.FactoryModule;
+import com.google.gerrit.server.events.Event;
 import com.googlesource.gerrit.plugins.chatgpt.config.Configuration;
+import com.googlesource.gerrit.plugins.chatgpt.mode.common.client.api.gerrit.GerritChange;
 
 public class GerritEventContextModule extends FactoryModule {
+    private final Event event;
     private final Configuration config;
 
-    public GerritEventContextModule(Configuration config) {
+    public GerritEventContextModule(Configuration config, Event event) {
+        this.event = event;
         this.config = config;
     }
 
     @Override
     protected void configure() {
-        install(EventHandlerTask.MODULE);
-
         bind(Configuration.class).toInstance(config);
+        bind(GerritChange.class).toInstance(new GerritChange(event));
     }
 }

src/main/java/com/googlesource/gerrit/plugins/chatgpt/listener/EventHandlerExecutor.java
@@ -32,9 +32,9 @@ public class EventHandlerExecutor {
     }
 
     public void execute(Configuration config, Event event) {
-        GerritEventContextModule contextModule = new GerritEventContextModule(config);
-        EventHandlerTask.Factory taskHandlerFactory = injector.createChildInjector(contextModule)
-                .getInstance(EventHandlerTask.Factory.class);
-        executor.execute(taskHandlerFactory.create(event));
+        GerritEventContextModule contextModule = new GerritEventContextModule(config, event);
+        EventHandlerTask task = injector.createChildInjector(contextModule)
+                .getInstance(EventHandlerTask.class);
+        executor.execute(task);
     }
 }

src/test/java/com/googlesource/gerrit/plugins/chatgpt/ChatGptReviewTestBase.java
@@ -205,10 +205,10 @@ public class ChatGptReviewTestBase {
         setupCommonEventMocks((PatchSetEvent) event); // Apply common mock configurations
         typeSpecificSetup.accept(event);
 
-        EventHandlerTask.Factory factory = Guice.createInjector(EventHandlerTask.MODULE, new AbstractModule() {
+        EventHandlerTask task = Guice.createInjector(new AbstractModule() {
             @Override
             protected void configure() {
-                install(new GerritEventContextModule(config));
+                install(new GerritEventContextModule(config, event));
 
                 bind(GerritClient.class).toInstance(gerritClient);
                 bind(GitRepoFiles.class).toInstance(gitRepoFiles);
@@ -216,8 +216,8 @@ public class ChatGptReviewTestBase {
                 bind(PatchSetReviewer.class).toInstance(patchSetReviewer);
                 bind(PluginDataHandler.class).toInstance(pluginDataHandler);
             }
-        }).getInstance(EventHandlerTask.Factory.class);
-        return factory.create(event).execute();
+        }).getInstance(EventHandlerTask.class);
+        return task.execute();
     }
 
     protected void testRequestSent() {

commit 524bbeb2199c31deee13ccaa10bd6bd08688987d

Fix regression: missing eventHandlerTask.MODULE
A regression introduced removing `eventHandlerTask.MODULE` is fixed by
re-introducing the module.

Fixes: ec1cf3b ("Bind GerritChange in event context")
Change-Id: I634de5a09bde7e0a3cfa2cb1e5b27618d0ae535b
Signed-off-by: Patrizio <patrizio.gelosi@amarulasolutions.com>

src/main/java/com/googlesource/gerrit/plugins/chatgpt/listener/EventHandlerTask.java
@@ -3,9 +3,12 @@ package com.googlesource.gerrit.plugins.chatgpt.listener;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Splitter;
 import com.google.gerrit.extensions.client.ChangeKind;
+import com.google.gerrit.extensions.config.FactoryModule;
 import com.google.gerrit.server.data.ChangeAttribute;
 import com.google.gerrit.server.data.PatchSetAttribute;
+import com.google.gerrit.server.events.Event;
 import com.google.inject.Inject;
+import com.google.inject.Module;
 import com.googlesource.gerrit.plugins.chatgpt.PatchSetReviewer;
 import com.googlesource.gerrit.plugins.chatgpt.config.Configuration;
 import com.googlesource.gerrit.plugins.chatgpt.mode.common.client.api.gerrit.GerritChange;
@@ -21,6 +24,16 @@ import static com.google.gerrit.extensions.client.ChangeKind.REWORK;
 
 @Slf4j
 public class EventHandlerTask implements Runnable {
+    public static final Module MODULE = new FactoryModule() {
+        @Override
+        protected void configure() {
+            factory(EventHandlerTask.Factory.class);
+        }
+    };
+    public interface Factory {
+        EventHandlerTask create(Event event);
+    }
+
     @VisibleForTesting
     public enum Result {
         OK, NOT_SUPPORTED, FAILURE

src/main/java/com/googlesource/gerrit/plugins/chatgpt/listener/GerritEventContextModule.java
@@ -26,6 +26,8 @@ public class GerritEventContextModule extends FactoryModule {
 
     @Override
     protected void configure() {
+        install(EventHandlerTask.MODULE);
+
         bind(IChatGptClient.class).to(getChatGptMode());
         bind(IGerritClientPatchSet.class).to(getClientPatchSet());
 