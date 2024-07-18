ebdc2d26178406891587ef070e70504a3761a911
Patrizio <patrizio.gelosi@amarulasolutions.com>
2024-07-18 09:20:22 +0200

    Enhance stateful requests with message references
    Added explicit references to the 'in-reply-to' message in stateful
    requests on Change Sets. Although these requests are context-aware due
    to being thread-scoped, it is not always straightforward for ChatGPT to
    determine the specific Gerrit message being responded to when multiple
    messages are involved. This update clarifies the message reference in
    such cases.
    
    Change-Id: I44969d19b8dfb542520377bbd9ff3ccaba5ef631
    Signed-off-by: Patrizio <patrizio.gelosi@amarulasolutions.com>

--- src/main/java/com/googlesource/gerrit/plugins/chatgpt/mode/stateful/client/prompt/ChatGptDataPromptRequestsStateful.java
+++ src/main/java/com/googlesource/gerrit/plugins/chatgpt/mode/stateful/client/prompt/ChatGptDataPromptRequestsStateful.java
@@ -4,10 +4,15 @@ import com.googlesource.gerrit.plugins.chatgpt.config.Configuration;
 import com.googlesource.gerrit.plugins.chatgpt.interfaces.mode.common.client.prompt.IChatGptDataPrompt;
 import com.googlesource.gerrit.plugins.chatgpt.localization.Localizer;
 import com.googlesource.gerrit.plugins.chatgpt.mode.common.client.prompt.ChatGptDataPromptRequests;
+import com.googlesource.gerrit.plugins.chatgpt.mode.common.model.api.chatgpt.ChatGptMessageItem;
 import com.googlesource.gerrit.plugins.chatgpt.mode.common.model.data.ChangeSetData;
 import com.googlesource.gerrit.plugins.chatgpt.mode.common.model.data.GerritClientData;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class ChatGptDataPromptRequestsStateful extends ChatGptDataPromptRequests implements IChatGptDataPrompt {
+    private static final String REPLY_MESSAGE_REFERENCE = " (Ref. message \"%s\")";
+
     public ChatGptDataPromptRequestsStateful(
             Configuration config,
             ChangeSetData changeSetData,
@@ -15,5 +20,25 @@ public class ChatGptDataPromptRequestsStateful extends ChatGptDataPromptRequests
             Localizer localizer
     ) {
         super(config, changeSetData, gerritClientData, localizer);
+        log.debug("ChatGptDataPromptRequestsStateful initialized");
+    }
+
+    @Override
+    protected ChatGptMessageItem getMessageItem(int i) {
+        log.debug("Getting stateful Message Item");
+        super.getMessageItem(i);
+        if (!messageHistory.isEmpty()) {
+            log.debug("Message History found: {}", messageHistory);
+            messageItem.appendToRequest(getReferenceToLastMessage());
+        }
+
+        return messageItem;
+    }
+
+    private String getReferenceToLastMessage() {
+        String referenceToLastMessage = String.format(REPLY_MESSAGE_REFERENCE,
+                messageHistory.get(messageHistory.size() - 1).getContent());
+        log.debug("Reference to last message: {}", referenceToLastMessage);
+        return referenceToLastMessage;
     }
 }
