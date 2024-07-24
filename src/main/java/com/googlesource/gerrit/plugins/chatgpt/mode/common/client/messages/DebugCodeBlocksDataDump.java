603b6bbb8cd17d2a10de57717af292f0c74d7ab2
Patrizio <patrizio.gelosi@amarulasolutions.com>
2024-07-24 16:53:50 +0200

    Introduce `dump_stored_data` command
    Added a new command that dumps stored data across all scope levels,
    enabling comprehensive visibility into the data for debugging and
    analysis purposes.
    
    Change-Id: I7a0f6dc1357464ec10efa30915b3461b34d0b2e7
    Signed-off-by: Patrizio <patrizio.gelosi@amarulasolutions.com>

---
+++ src/main/java/com/googlesource/gerrit/plugins/chatgpt/mode/common/client/messages/DebugCodeBlocksDataDump.java
@@ -0,0 +1,48 @@
+package com.googlesource.gerrit.plugins.chatgpt.mode.common.client.messages;
+
+import com.googlesource.gerrit.plugins.chatgpt.data.PluginDataHandler;
+import com.googlesource.gerrit.plugins.chatgpt.data.PluginDataHandlerProvider;
+import com.googlesource.gerrit.plugins.chatgpt.localization.Localizer;
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.googlesource.gerrit.plugins.chatgpt.utils.StringUtils.convertPascalCaseToWords;
+import static com.googlesource.gerrit.plugins.chatgpt.utils.TextUtils.prettyStringifyMap;
+
+@Slf4j
+public class DebugCodeBlocksDataDump extends DebugCodeBlocks {
+    private final List<String> dataDump = new ArrayList<>();
+
+    public DebugCodeBlocksDataDump(Localizer localizer, PluginDataHandlerProvider pluginDataHandlerProvider) {
+        super(localizer.getText("message.dump.stored.data.title"));
+        retrieveStoredData(pluginDataHandlerProvider);
+    }
+
+    public String getDataDumpBlock() {
+        return super.getDebugCodeBlock(dataDump);
+    }
+
+    private void retrieveStoredData(PluginDataHandlerProvider pluginDataHandlerProvider) {
+        for (Method method : pluginDataHandlerProvider.getClass().getDeclaredMethods()) {
+            method.setAccessible(true);
+            try {
+                String methodName = method.getName();
+                log.debug("Retrieving stored method {}", methodName);
+                if (!methodName.startsWith("get") || !methodName.endsWith("Scope")) continue;
+                String dataKey = methodName.replaceAll("^get", "");
+                log.debug("Populating data key {}", dataKey);
+                dataDump.add("### " + convertPascalCaseToWords(dataKey));
+                PluginDataHandler dataHandler = (PluginDataHandler) method.invoke(pluginDataHandlerProvider);
+                log.debug("Retrieved Data Handler {}", dataHandler);
+                dataDump.add(prettyStringifyMap(dataHandler.getAllValues()) + "\n");
+            }
+            catch (Exception e) {
+                log.error("Error while invoking method: {}", method.getName(), e);
+                throw new RuntimeException("Error while retrieving stored data", e);
+            }
+        }
+    }
+}
