28454c45911fe4c3eb84d5887199da850e7332da
Patrizio <patrizio.gelosi@amarulasolutions.com>
2024-07-31 17:11:13 +0200

    Resolve outdated message issues in ChatGPT reviews
    In stateful mode, when new Patch Sets are submitted within the same
    Change Set, the ChatGPT review process may incorrectly reiterate
    responses pertaining to issues already resolved.
    This fix creates a new ChatGPT review thread with each new Patch Set
    commit to prevent such outdated messages.
    
    Change-Id: Iccd76d8cd4ce5ea9ab0368d9c3cf9b9ca3223cd1
    Signed-off-by: Patrizio <patrizio.gelosi@amarulasolutions.com>

--- src/main/java/com/googlesource/gerrit/plugins/chatgpt/mode/stateful/client/api/chatgpt/ChatGptThread.java
+++ src/main/java/com/googlesource/gerrit/plugins/chatgpt/mode/stateful/client/api/chatgpt/ChatGptThread.java
@@ -3,6 +3,7 @@ package com.googlesource.gerrit.plugins.chatgpt.mode.stateful.client.api.chatgpt
 import com.googlesource.gerrit.plugins.chatgpt.config.Configuration;
 import com.googlesource.gerrit.plugins.chatgpt.data.PluginDataHandler;
 import com.googlesource.gerrit.plugins.chatgpt.data.PluginDataHandlerProvider;
+import com.googlesource.gerrit.plugins.chatgpt.mode.common.model.data.ChangeSetData;
 import com.googlesource.gerrit.plugins.chatgpt.mode.stateful.client.api.UriResourceLocatorStateful;
 import com.googlesource.gerrit.plugins.chatgpt.mode.stateful.model.api.chatgpt.ChatGptResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -18,19 +19,22 @@ public class ChatGptThread {
 
     private final ChatGptHttpClient httpClient = new ChatGptHttpClient();
     private final Configuration config;
+    private final ChangeSetData changeSetData;
     private final PluginDataHandler changeDataHandler;
 
     public ChatGptThread(
             Configuration config,
+            ChangeSetData changeSetData,
             PluginDataHandlerProvider pluginDataHandlerProvider
     ) {
         this.config = config;
+        this.changeSetData = changeSetData;
         this.changeDataHandler = pluginDataHandlerProvider.getChangeScope();
     }
 
     public String createThread() {
         String threadId = changeDataHandler.getValue(KEY_THREAD_ID);
-        if (threadId == null) {
+        if (threadId == null || !changeSetData.getForcedReview()) {
             Request request = createThreadRequest();
             log.debug("ChatGPT Create Thread request: {}", request);
 
