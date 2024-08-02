650b6060543f5fb80a12a975d220b03e43e76103
Patrizio <patrizio.gelosi@amarulasolutions.com>
2024-08-02 16:17:52 +0200

    Introduce `message` command
    After introducing strict command validation in a previous commit,
    unknown commands now trigger error messages and are neither processed
    nor forwarded to ChatGPT. This could affect messages intended for
    ChatGPT that inadvertently include slashes followed by alphanumeric
    strings, interpreted as commands. The `/message` command is introduced
    to ensure such messages are forwarded to ChatGPT correctly, bypassing
    command parsing.
    
    Change-Id: I81e47d60c462e8735fbc6b71fc76382a97487b0b
    Signed-off-by: Patrizio <patrizio.gelosi@amarulasolutions.com>

--- src/main/java/com/googlesource/gerrit/plugins/chatgpt/mode/common/client/commands/ClientCommandBase.java
+++ src/main/java/com/googlesource/gerrit/plugins/chatgpt/mode/common/client/commands/ClientCommandBase.java
@@ -2,6 +2,7 @@ package com.googlesource.gerrit.plugins.chatgpt.mode.common.client.commands;
 
 import com.googlesource.gerrit.plugins.chatgpt.config.Configuration;
 import com.googlesource.gerrit.plugins.chatgpt.mode.common.client.ClientBase;
+import com.google.common.collect.ImmutableBiMap;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.regex.Pattern;
@@ -9,6 +10,7 @@ import java.util.regex.Pattern;
 @Slf4j
 public abstract class ClientCommandBase extends ClientBase {
     public enum CommandSet {
+        MESSAGE,
         REVIEW,
         REVIEW_LAST,
         DIRECTIVES,
@@ -25,9 +27,21 @@ public abstract class ClientCommandBase extends ClientBase {
         CONFIGURATION_OPTION
     }
 
+    protected static final ImmutableBiMap<String, CommandSet> COMMAND_MAP = ImmutableBiMap.of(
+            "message", CommandSet.MESSAGE,
+            "review", CommandSet.REVIEW,
+            "review_last", CommandSet.REVIEW_LAST,
+            "directives", CommandSet.DIRECTIVES,
+            "forget_thread", CommandSet.FORGET_THREAD,
+            "configure", CommandSet.CONFIGURE,
+            "dump_stored_data", CommandSet.DUMP_STORED_DATA
+    );
+
     // Option values can be either a sequence of chars enclosed in double quotes or a sequence of non-space chars.
     private static final String OPTION_VALUES = "\"[^\"\\\\]*(?:\\\\.[^\"\\\\]*)*\"|\\S+";
 
+    protected static final Pattern MESSAGE_COMMAND_PATTERN = Pattern.compile("\\s*/" +
+            COMMAND_MAP.inverse().get(CommandSet.MESSAGE) + "\\b(.*)$");
     protected static final Pattern COMMAND_PATTERN = Pattern.compile("/(\\w+)\\b((?:\\s+--\\w+(?:=(?:" +
             OPTION_VALUES + "))?)+)?");
     protected static final Pattern OPTIONS_PATTERN = Pattern.compile("--(\\w+)(?:=(" + OPTION_VALUES + "))?");
