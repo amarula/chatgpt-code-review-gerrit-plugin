d94018e09da97872d2152a9e2578c8441d15ee68
Patrizio <patrizio.gelosi@amarulasolutions.com>
2024-08-05 09:49:54 +0200

    Introduce `dump_config` command
    The new `/dump_config` command has been added to allow the dumping of
    relevant non-confidential configuration settings, not limited to those
    dynamically changed. This command is active only when
    `enableMessageDebugging` config setting is enabled.
    
    Change-Id: Id2605507c0772fd4a598da8ee94b90d91d912d8f
    Signed-off-by: Patrizio <patrizio.gelosi@amarulasolutions.com>

---
+++ src/main/java/com/googlesource/gerrit/plugins/chatgpt/mode/common/client/messages/DebugCodeBlocksConfiguration.java
@@ -0,0 +1,28 @@
+package com.googlesource.gerrit.plugins.chatgpt.mode.common.client.messages;
+
+import com.googlesource.gerrit.plugins.chatgpt.config.Configuration;
+import com.googlesource.gerrit.plugins.chatgpt.localization.Localizer;
+
+import java.util.List;
+import java.util.TreeMap;
+
+import static com.googlesource.gerrit.plugins.chatgpt.utils.TextUtils.prettyStringifyMap;
+
+public class DebugCodeBlocksConfiguration extends DebugCodeBlocksBase {
+     private final Localizer localizer;
+
+    public DebugCodeBlocksConfiguration(Localizer localizer) {
+        super(localizer.getText("message.dump.configuration.title"));
+        this.localizer = localizer;
+    }
+
+    public String getDataDumpBlock(Configuration config) {
+        TreeMap<String, String> configMap = config.dumpConfigMap();
+        if (configMap == null) {
+            return localizer.getText("message.dump.configuration.error");
+        }
+        return super.getDebugCodeBlock(List.of(
+                prettyStringifyMap(configMap)
+        ));
+    }
+}
