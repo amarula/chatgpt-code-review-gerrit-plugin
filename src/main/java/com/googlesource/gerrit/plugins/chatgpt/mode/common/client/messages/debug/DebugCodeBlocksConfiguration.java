9b0e4a1f1fe461c47585b9eb2eb41f86a695b0fd
Patrizio <patrizio.gelosi@amarulasolutions.com>
2024-08-20 15:36:42 +0200

    Refactor debugging classes
    Classes specifically used for composing debugging messages have been
    moved to the new `messages.debug` package for better organization and
    maintainability.
    Additionally, the function responsible for returning the debug code
    block has been standardized to `getDebugCodeBlock`.
    
    Change-Id: I002264cf52b8488c983b6188f8c7173165eb376d
    Signed-off-by: Patrizio <patrizio.gelosi@amarulasolutions.com>

--- src/main/java/com/googlesource/gerrit/plugins/chatgpt/mode/common/client/messages/debug/DebugCodeBlocksConfiguration.java
+++ src/main/java/com/googlesource/gerrit/plugins/chatgpt/mode/common/client/messages/debug/DebugCodeBlocksConfiguration.java
@@ -1,4 +1,4 @@
-package com.googlesource.gerrit.plugins.chatgpt.mode.common.client.messages;
+package com.googlesource.gerrit.plugins.chatgpt.mode.common.client.messages.debug;
 
 import com.googlesource.gerrit.plugins.chatgpt.config.Configuration;
 import com.googlesource.gerrit.plugins.chatgpt.localization.Localizer;
@@ -9,14 +9,14 @@ import java.util.TreeMap;
 import static com.googlesource.gerrit.plugins.chatgpt.utils.JsonTextUtils.prettyStringifyMap;
 
 public class DebugCodeBlocksConfiguration extends DebugCodeBlocksBase {
-     private final Localizer localizer;
+    private final Localizer localizer;
 
     public DebugCodeBlocksConfiguration(Localizer localizer) {
         super(localizer.getText("message.dump.configuration.title"));
         this.localizer = localizer;
     }
 
-    public String getDataDumpBlock(Configuration config) {
+    public String getDebugCodeBlock(Configuration config) {
         TreeMap<String, String> configMap = config.dumpConfigMap();
         if (configMap == null) {
             return localizer.getText("message.dump.configuration.error");
