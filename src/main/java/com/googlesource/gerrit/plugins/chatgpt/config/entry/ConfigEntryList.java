037503f6e570177b43a088d883702ac1f06f2a1f
Patrizio <patrizio.gelosi@amarulasolutions.com>
2024-08-22 15:09:22 +0200

    Redesign directives config setting as a list
    Directives have been redesigned into a pure String List configuration
    setting.
    With this change, multiple directives can now be added to configuration
    files using multiple `directive = ...` entries, leading to the renaming
    of the `directives` setting to `directive`.
    Additionally, the `/directives` command has been updated from an alias
    for `/configure --directives` to a standalone command for managing
    dynamic directives. Dynamic directives can now be queried, added, or
    removed using `/directives`.
    Finally, dynamic directives are no longer overridden by project-scoped
    directives but are appended to them instead.
    
    Change-Id: I628d40aff3e118ae04492054e741c557fb7ebb51
    Signed-off-by: Patrizio <patrizio.gelosi@amarulasolutions.com>

---
+++ src/main/java/com/googlesource/gerrit/plugins/chatgpt/config/entry/ConfigEntryList.java
@@ -0,0 +1,31 @@
+package com.googlesource.gerrit.plugins.chatgpt.config.entry;
+
+import com.google.gerrit.server.config.PluginConfig;
+import com.googlesource.gerrit.plugins.chatgpt.interfaces.config.entry.IConfigEntry;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static com.googlesource.gerrit.plugins.chatgpt.utils.JsonTextUtils.jsonArrayToList;
+
+public class ConfigEntryList extends ConfigEntryBase implements IConfigEntry {
+
+    public ConfigEntryList(String key) {
+        super(key);
+    }
+
+    public void setCurrentConfigValue(Map<String, Object> configValues, PluginConfig projectConfig) {
+        configValues.put(key, new ArrayList<>(Arrays.asList(projectConfig.getStringList(key))));
+    }
+
+    public void setDynamicConfigValue(Map<String, Object> configValues, String value) {
+        configValues.put(key, jsonArrayToList(value));
+    }
+
+    @SuppressWarnings("unchecked")
+    public void setMergedConfigValue(PluginConfig.Update configUpdater, Object value) {
+        configUpdater.setStringList(key, (List<String>) value);
+    }
+}
