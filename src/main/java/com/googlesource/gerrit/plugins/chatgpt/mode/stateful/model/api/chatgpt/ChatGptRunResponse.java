d3090a2db9321a6c10aa5d7fd7447e4b3f5d16dc
Patrizio <patrizio.gelosi@amarulasolutions.com>
2024-10-07 15:09:03 +0200

    Enhance logging for ChatGptResponse inheritors
    Logging of data class instances that inherit from ChatGptResponse has
    been improved by including the parent class properties (`id`, `object`,
    and `status`).
    
    Change-Id: I686f4ee7f6866c77c7167310f72cd4701fa1511e
    Signed-off-by: Patrizio <patrizio.gelosi@amarulasolutions.com>

--- src/main/java/com/googlesource/gerrit/plugins/chatgpt/mode/stateful/model/api/chatgpt/ChatGptRunResponse.java
+++ src/main/java/com/googlesource/gerrit/plugins/chatgpt/mode/stateful/model/api/chatgpt/ChatGptRunResponse.java
@@ -4,11 +4,13 @@ import com.google.gson.annotations.SerializedName;
 import com.googlesource.gerrit.plugins.chatgpt.mode.common.model.api.chatgpt.ChatGptToolCall;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
+@ToString(callSuper = true)
 public class ChatGptRunResponse extends ChatGptResponse {
     @SerializedName("required_action")
     private RequiredAction requiredAction;
