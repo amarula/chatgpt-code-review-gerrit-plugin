816582cab1fb6b289db912dad4d8deceeac63a3e
Patrizio <patrizio.gelosi@amarulasolutions.com>
2024-10-07 15:09:03 +0200

    Enhance logging for ChatGptResponse inheritors
    Logging of data class instances that inherit from ChatGptResponse has
    been improved by including the parent class properties (`id`, `object`,
    and `status`).
    
    Change-Id: I686f4ee7f6866c77c7167310f72cd4701fa1511e
    Signed-off-by: Patrizio <patrizio.gelosi@amarulasolutions.com>

--- src/main/java/com/googlesource/gerrit/plugins/chatgpt/mode/stateful/model/api/chatgpt/ChatGptFilesResponse.java
+++ src/main/java/com/googlesource/gerrit/plugins/chatgpt/mode/stateful/model/api/chatgpt/ChatGptFilesResponse.java
@@ -2,9 +2,11 @@ package com.googlesource.gerrit.plugins.chatgpt.mode.stateful.model.api.chatgpt;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
+@ToString(callSuper = true)
 public class ChatGptFilesResponse extends ChatGptResponse {
     private String filename;
 }
